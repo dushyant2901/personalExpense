0b8cc7746d7d47fb1fbcf97c49ad1ace942e2cae
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-01-15 12:52:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const input= document.querySelector("#amount");
+const btn = document.querySelector("#btn");
+const total = document.querySelector("#total");
+const desc= document.querySelector("#desc");
+let expense=0;
+let all=[]
+
+function btnListen(){
+    let obj={}
+    let userInput=input.value
+    let userDesc=desc.value
+    console.log({userInput})
+    let uexpense=parseInt(userInput,10)
+    console.log({uexpense})
+    console.log({userDesc})
+     expense=expense+userInput
+    console.log({expense})
+    
+    obj.desc=userDesc
+    obj.amt=userInput
+
+    all.push(obj)
+    console.log({all})
+
+    total.innerText="The Total Expense is ${expense}"
+
+}
+
+
+btn.addEventListener("click",btnListen)
