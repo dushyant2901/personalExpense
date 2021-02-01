5cc1e7b954c3ab14622a73d01be0bc876514f456
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-02-01 22:00:10 +0530

    feat tabular data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const input= document.querySelector("#amount");
 const btn = document.querySelector("#btn");
 const total = document.querySelector("#total");
 const desc= document.querySelector("#desc");
+const data=document.querySelector("#output")
 let expense=0;
 let all=[]
 
@@ -22,6 +23,13 @@ function btnListen(){
   
  total.textContent=`The Total Expense is :${ expense}`
 
+ data1=all[0]
+ data2=all[1]
+ 
+ data3=`Expense:${data1.desc} :: amount:${data1.amt}`
+ 
+ data4=`Expense:${data2.desc} :: amount:${data2.amt}`
+data.innerText=<li> {data3+data4}</li>
 }
 
 
