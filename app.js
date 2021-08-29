7f6b3649b9f2dfe5f70f1103d83a874dee67f1bd
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-29 11:51:29 +0530

    refracted code

--- app.js
+++ app.js
@@ -2,34 +2,35 @@ const input= document.querySelector("#amount");
 const btn = document.querySelector("#btn");
 const total = document.querySelector("#total");
 const desc= document.querySelector("#desc");
-const data=document.querySelector("#output")
+const data=document.querySelector("#output");
 let expense=0;
-let all=[]
+let all=[];
 
 function btnListen(){
-    let obj={}
-    let userInput=input.value
-    let userDesc=desc.value
+    let obj={};
+    let userInput=input.value;
+    let userDesc=desc.value;
    
-    let ueExpense=parseInt(userInput, 10)
+    let ueExpense=parseInt(userInput, 10);
   
-    expense=expense+ueExpense
+    expense=expense+ueExpense;
     
     
-    obj.desc=userDesc
-    obj.amt=ueExpense
+    obj.desc=userDesc;
+    obj.amt=ueExpense;
 
-    all.push(obj)
+    all.push(obj);
   
- total.textContent=`The Total Expense is :${ expense}`
+ total.textContent=`The Total Expense is :${ expense}`;
 
- data1=all[0]
- data2=all[1]
+ data1=all[0];
+ data2=all[1];
  
- data3=`Expense:${data1.desc} :: amount:${data1.amt}`
+ data3=`Expense:${data1.desc} :: amount:${data1.amt}`;
  
- data4=`Expense:${data2.desc} :: amount:${data2.amt}`
-data.innerText=<li> {data3+data4}</li>
+ data4=`Expense:${data2.desc} :: amount:${data2.amt}`;
+
+data.innerText=<li> {data3+data4}</li>;
 }
 
 
